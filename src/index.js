40462c5ae938efe127c36dc6434b1c18d0c77043
Moudy <moudy.elkammash@gmail.com>
2017-10-06 12:18:08 -0400

    Use prop-types

--- src/index.js
+++ src/index.js
@@ -1,39 +1,42 @@
-import Autolinker from 'autolinker';
-import React from 'react';
+import Autolinker from "autolinker";
+import React from "react";
+import PropTypes from "prop-types";
 
 export default class ReactAutolinker extends React.Component {
   static propTypes = {
-    options: React.PropTypes.object,
-    renderLink: React.PropTypes.func
-  }
+    options: PropTypes.object,
+    renderLink: PropTypes.func
+  };
   static defaultProps = {
     options: {},
-    renderLink: (tag) => React.createElement(tag.tagName, tag.attrs, tag.innerHtml)
-  }
+    renderLink: tag =>
+      React.createElement(tag.tagName, tag.attrs, tag.innerHtml)
+  };
   render() {
-    const {
-      options,
-      text,
-      renderLink
-    } = this.props;
+    const { options, text, renderLink } = this.props;
 
     const tags = [];
-    Autolinker.link(text, {...options, replaceFn: (autolinker, match) => {
-      const tag = autolinker.getTagBuilder().build(match);
-      tags.push(tag);
-      return tag;
-    }});
+    Autolinker.link(text, {
+      ...options,
+      replaceFn: (autolinker, match) => {
+        const tag = autolinker.getTagBuilder().build(match);
+        tags.push(tag);
+        return tag;
+      }
+    });
 
     let _text = text;
     const children = [];
-    for(let tag of tags) {
-      const splitText = _text.includes(tag.attrs.href) ? tag.attrs.href : tag.innerHtml;
+    for (let tag of tags) {
+      const splitText = _text.includes(tag.attrs.href)
+        ? tag.attrs.href
+        : tag.innerHtml;
       const parts = _text.split(splitText);
       if (tag.attrs && tag.attrs.class) {
         tag.attrs.className = tag.attrs.class;
         delete tag.attrs.class;
       }
-      tag.attrs.key = `${tag.attrs.href}-${tags.indexOf(tag)}`
+      tag.attrs.key = `${tag.attrs.href}-${tags.indexOf(tag)}`;
       children.push(parts.shift());
       children.push(renderLink(tag));
       _text = parts.join(tag.attrs.href);
@@ -41,10 +44,9 @@ export default class ReactAutolinker extends React.Component {
 
     const content = children.length ? children : text;
     const props = { ...this.props };
-    for (let prop of ['text', 'options', 'renderLink']) {
+    for (let prop of ["text", "options", "renderLink"]) {
       delete props[prop];
     }
-    return React.createElement('div', props, content);
+    return React.createElement("div", props, content);
   }
 }
-
